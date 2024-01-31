027a9127610410f08ab6b831af94e3c9f14fd70f
Mbonu Basil <mbonubasil@gmail.com>
2024-01-31 18:55:32 -0500

    Add Order and OrderItem entities

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,38 @@
-export class Order {}
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  createdAt: Date;
+
+  @Column({ default: null })
+  fulfilledAt: Date;
+
+  @OneToMany(() => OrderItem, (orderItem) => orderItem.order, { cascade: true })
+  orderItems: OrderItem[];
+}
+
+@Entity()
+export class OrderItem {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  pharmacy: string;
+
+  @Column()
+  product: string;
+
+  @ManyToOne(() => Order, (order) => order.orderItems)
+  order: Order;
+}
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { Order, OrderItem } from './entities/order.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
   controllers: [OrderController],
   providers: [OrderService],
 })
