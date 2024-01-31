9e06e336ba5332498196c823a234913b27e38fdd
Mbonu Basil <mbonubasil@gmail.com>
2024-01-31 18:53:27 -0500

    Setup sqlite in memory db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { OrderModule } from './order/order.module';
+import { Order, OrderItem } from './order/entities/order.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    OrderModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sql',
+      synchronize: true,
+      entities: [OrderItem, Order],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
